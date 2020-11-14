d1e557a93fbd43f354a558ee8d9b610a7d15bcac
Gui Goncalves <gsg@ggoncalves.me>
2020-11-14 15:28:16 +0000

    Make the TerminalDisplayer write to any io.Writer

--- display/terminal.go
+++ display/terminal.go
@@ -2,44 +2,45 @@ package display
 
 import (
 	"fmt"
+	"io"
 	"mazes/core"
 )
 
 type TerminalDisplayer struct {
-
+	writer io.Writer
 }
 
-func MakeTerminalDisplay() TerminalDisplayer {
-	return TerminalDisplayer{}
+func MakeTerminalDisplay(writer io.Writer) TerminalDisplayer {
+	return TerminalDisplayer{writer}
 }
 
 func (d TerminalDisplayer) Display(g core.Grid) {
-	printUpperLine(g)
-	printRows(g)
-	printBottomLine(g)
-	fmt.Println()
+	d.printUpperLine(g)
+	d.printRows(g)
+	d.printBottomLine(g)
+	fmt.Fprintln(d.writer)
 }
 
-func printUpperLine(g core.Grid) {
+func (d TerminalDisplayer) printUpperLine(g core.Grid) {
 	columns := ""
 	for i := 0; i < g.Columns-1; i++ {
 		columns += "───┬"
 	}
-	fmt.Printf("┌%s───┐\n", columns)
+	fmt.Fprintf(d.writer, "┌%s───┐\n", columns)
 }
 
-func printRows(g core.Grid) {
+func (d TerminalDisplayer) printRows(g core.Grid) {
 	g.EachRow(func(cells []*core.Cell, _ int) {
-		fmt.Print("│")
+		fmt.Fprint(d.writer, "│")
 		bottomRow := "├"
 
 		for i := 0; i < len(cells); i++ {
 			cell := cells[i]
-			fmt.Print("   ")
+			fmt.Fprint(d.writer, "   ")
 			if cell.East != nil && cell.IsLinked(*cell.East) {
-				fmt.Print(" ")
+				fmt.Fprint(d.writer, " ")
 			} else {
-				fmt.Print("│")
+				fmt.Fprint(d.writer, "│")
 			}
 			if cell.South != nil && cell.IsLinked(*cell.South) {
 				bottomRow += "   "
@@ -54,17 +55,17 @@ func printRows(g core.Grid) {
 				}
 			}
 		}
-		fmt.Println()
+		fmt.Fprintln(d.writer)
 		if cells[0].South != nil {
-			fmt.Println(bottomRow)
+			fmt.Fprintln(d.writer, bottomRow)
 		}
 	})
 }
 
-func printBottomLine(g core.Grid) {
-	fmt.Print("└")
+func (d TerminalDisplayer) printBottomLine(g core.Grid) {
+	fmt.Fprint(d.writer, "└")
 	for i := 0; i < g.Columns-1; i++ {
-		fmt.Print("───┴")
+		fmt.Fprint(d.writer, "───┴")
 	}
-	fmt.Print("───┘")
+	fmt.Fprint(d.writer, "───┘")
 }
